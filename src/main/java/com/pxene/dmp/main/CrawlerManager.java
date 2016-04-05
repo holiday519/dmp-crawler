824c00a06805bb7dd7208728e6986d32eaa2c600
zhengyi <zhengyi19870519@gmail.com>
2016-04-05 18:14:05 +0800

    bug

--- src/main/java/com/pxene/dmp/main/CrawlerManager.java
+++ src/main/java/com/pxene/dmp/main/CrawlerManager.java
@@ -17,7 +17,7 @@ import edu.uci.ics.crawler4j.robotstxt.RobotstxtConfig;
 import edu.uci.ics.crawler4j.robotstxt.RobotstxtServer;
 
 public class CrawlerManager {
-	private static final String USERAGENT="Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36";
+	private static final String USERAGENT = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36";
 	public static void main(String[] args) throws Exception {
 		// 基本配置
 		final String packageName = "com.pxene.dmp.crawler";
@@ -28,36 +28,39 @@ public class CrawlerManager {
 		options.addOption("className", true, "input class name of crawler");
 		CommandLineParser parser = new BasicParser();
 		CommandLine line = parser.parse(options, args);
-		
+
 		if (line.hasOption("className")) {
 			final String className = line.getOptionValue("className");
 			CrawlConfig config = new CrawlConfig();
-	        config.setCrawlStorageFolder(crawlStorageFolder);
-	        config.setUserAgentString(USERAGENT);
-	        
-	        PageFetcher pageFetcher = new PageFetcher(config);
-	        RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
-	        robotstxtConfig.setEnabled(false); //关闭robot协议
-	        robotstxtConfig.setUserAgentName(USERAGENT);
-	        robotstxtConfig.setUserAgentName("");
-	        RobotstxtServer robotstxtServer = new RobotstxtServer(robotstxtConfig, pageFetcher);
-	        CrawlController controller = new CrawlController(config, pageFetcher, robotstxtServer);
-	        
-	        for (String seed : SeedConfig.getSeeds(className)) {
-	        	controller.addSeed(seed);
-	        }
-	        
-	        controller.start(new WebCrawlerFactory<WebCrawler>() {
+			config.setCrawlStorageFolder(crawlStorageFolder);
+			config.setUserAgentString(USERAGENT);
+
+			PageFetcher pageFetcher = new PageFetcher(config);
+			RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
+			// 关闭robot协议
+			robotstxtConfig.setEnabled(false);
+			robotstxtConfig.setUserAgentName(USERAGENT);
+			RobotstxtServer robotstxtServer = new RobotstxtServer(
+					robotstxtConfig, pageFetcher);
+			CrawlController controller = new CrawlController(config,
+					pageFetcher, robotstxtServer);
+
+			for (String seed : SeedConfig.getSeeds(className)) {
+				controller.addSeed(seed);
+			}
+
+			controller.start(new WebCrawlerFactory<WebCrawler>() {
 				@Override
 				public WebCrawler newInstance() throws Exception {
-					return (WebCrawler)Class.forName(packageName + "." + className).newInstance();
+					return (WebCrawler) Class.forName(
+							packageName + "." + className).newInstance();
 				}
 			}, numberOfCrawlers);
 		} else {
 			HelpFormatter formatter = new HelpFormatter();
-            formatter.printHelp("options", options);
+			formatter.printHelp("options", options);
 		}
-		
+
 	}
-	
+
 }
