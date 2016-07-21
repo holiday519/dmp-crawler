816d7ac107f7a1644a8c3382193d7a60c319734a
Wei Guozhu <weiguozhu@pxene.com>
2016-07-21 19:13:11 +0800

    weibo爬虫代码

---
+++ src/main/java/com/pxene/dmp/crawler/auto/Crawler4Weibo.java
@@ -0,0 +1,86 @@
+package com.pxene.dmp.crawler.auto;
+
+import java.util.Iterator;
+import java.util.regex.Pattern;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import com.pxene.dmp.common.StringUtils;
+import com.pxene.dmp.crawler.BaseCrawler;
+
+import edu.uci.ics.crawler4j.crawler.Page;
+import edu.uci.ics.crawler4j.url.WebURL;
+
+public class Crawler4Weibo extends BaseCrawler {
+	public Crawler4Weibo() {
+		super("/" + Crawler4Weibo.class.getName().replace(".", "/") + ".json");
+	}
+
+	private Log logger = LogFactory.getLog(Crawler4Weibo.class);
+
+	private final static Pattern FILTERS = Pattern
+			.compile(".*(\\.(css|js|bmp|gif|jpe?g"
+					+ "|png|tiff?|mid|mp2|mp3|mp4"
+					+ "|wav|avi|mov|mpeg|ram|m4v|pdf"
+					+ "|rm|smil|wmv|swf|wma|zip|rar|gz))$");
+	private static final String CONTAINS = "^http://weibo.com/ttarticle/p/show?id=([0-9]+)$|^http://weibo.com/?category=([0-9]+)$";
+	private static final String TEXT = "text";
+
+	@Override
+	protected void onContentFetchError(WebURL webUrl) {
+		if (proxyConf.isEnable()) {
+			String[] params = proxyConf.randomIp().split(":");
+			System.getProperties().setProperty("proxySet", "true");
+			System.getProperties().setProperty("http.proxyHost", params[0]);
+			System.getProperties().setProperty("http.proxyPort", params[1]);
+		}
+	}
+
+	@Override
+	public boolean shouldVisit(Page referringPage, WebURL url) {
+		String href = url.getURL().toLowerCase();
+		return !FILTERS.matcher(href).matches();//&& href.matches(CONTAINS);
+	}
+
+	@Override
+	public void visit(Page page) {
+		String url = page.getWebURL().getURL();
+		System.out.println("=============================url:" + url);
+	}
+
+	/**
+	 * 根据选择器获得相应的内容
+	 * 
+	 * @param doc
+	 * @param selector
+	 * @param attribute
+	 * @param regexp
+	 * @return
+	 */
+	private String getTextBySelector(Document doc, String selector,
+			String attribute, String regexp) {
+		String text = "";
+		Elements elements = doc.select(selector);
+		Iterator<Element> iterator = elements.iterator();
+		while (iterator.hasNext()) {
+			Element element = iterator.next();
+			String element_comment = "";
+			if (attribute.equals(TEXT)) {
+				element_comment = element.text();
+			} else {
+				element_comment = element.attr(attribute);
+			}
+			if (org.apache.commons.lang.StringUtils.isNotBlank(regexp)) {
+				text = StringUtils.regexpExtract(element_comment, regexp);
+			} else {
+				text = element_comment;
+			}
+		}
+		return text.trim();
+	}
+
+}
